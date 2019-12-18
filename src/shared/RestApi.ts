02fa3e40b730cb3f74ac3f149e775b2814dd0c94
Andres Aguirre <aguirregzz97@gmail.com>
2019-12-17 18:28:16 -0600

    Feat[components] Add RestApi file to handle endpoints

--- src/shared/RestApi.ts
+++ src/shared/RestApi.ts
@@ -1,6 +1,15 @@
-import { authToken } from "./Constants"
+import { authToken } from './Constants'
+import { ActivityPlan } from './Types'
 
-export async function getActivities() {
+interface IActivityPlansResponseData {
+  data: IActivityPlanData
+}
+
+interface IActivityPlanData {
+  activity_plan: ActivityPlan
+}
+
+export async function getActivities() : Promise<ActivityPlan> {
   try {
     const response = await fetch('http://demo.kinedu.com/api/v3/babies/1/activity_plans/current', {
       method: 'GET',
@@ -9,8 +18,8 @@ export async function getActivities() {
         'Authorization': `${authToken}`,
       }
     })
-    const res = await response.json()
-    return res
+    const res : IActivityPlansResponseData = await response.json()
+    return res.data.activity_plan
   } 
   catch (error) {
     return error
